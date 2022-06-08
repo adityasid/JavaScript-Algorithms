d5e3dbae6428c33ebbd5bdea94857150bdfc3f1f
AdityaSid <adityasid786@gmail.com>
2022-06-08 08:12:33 +0530

    optimized solution for check is prime number or not

--- 2.Prime-Number.js
+++ 2.Prime-Number.js
@@ -4,7 +4,7 @@ function isPrime(n) {
         return false;
     }
 
-    for (let i = 2; i < n; i++) {
+    for (let i = 2; i <= Math.sqrt(n); i++) {
         if (n % i === 0) {
             return false;
         }
@@ -20,4 +20,4 @@ console.log(isPrime(4)); // false
 console.log(isPrime(5)); // true
 console.log(isPrime(6)); // false
 
-// Big-O = O(n)
+// Big-O = O(sqrt(n))
