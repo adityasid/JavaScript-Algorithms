f05445a30189dd63987e4d2e3fc284a30a384e8b
AdityaSid <adityasid786@gmail.com>
2022-06-09 05:33:04 +0530

    bitwise solution for power of two problem

--- 4.Power-of-two.js
+++ 4.Power-of-two.js
@@ -14,3 +14,19 @@ console.log(isPowerOfTwo(3)); // false
 console.log(isPowerOfTwo(4)); // true
 
 // Big-O = O(log n)
+
+/////////////////////////////
+console.log("bitwise solution");
+
+
+function isPowerOfTwoBitwise(n) {
+    if (n < 1) {
+        return false;
+    }
+    return (n & (n - 1)) === 0;
+}
+
+console.log(isPowerOfTwoBitwise(1)); // true
+console.log(isPowerOfTwoBitwise(2)); // true
+console.log(isPowerOfTwoBitwise(3)); // false
+console.log(isPowerOfTwoBitwise(4)); // true
