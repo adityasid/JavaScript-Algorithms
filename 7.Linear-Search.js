9c62374127fecef711ed6b866334d1c8b1a65443
AdityaSid <adityasid786@gmail.com>
2022-06-10 23:06:41 +0530

    Linear search

---
+++ 7.Linear-Search.js
@@ -0,0 +1,21 @@
+function linearSearch(arr, target) {
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === target) {
+            return i;
+            break;
+        }
+    }
+
+    return -1;
+
+}
+
+let arr = [-4, 6, 3, 2, 9, 7, 2];
+
+console.log(linearSearch(arr, 2));  // 3
+console.log(linearSearch(arr, 9));  // 4
+console.log(linearSearch(arr, -4)); // 0
+console.log(linearSearch(arr, 20)); // -1
+
+// Big-O = O(n)
