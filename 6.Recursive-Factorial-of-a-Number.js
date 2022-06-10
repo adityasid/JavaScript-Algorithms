5f0875ec235a6ecae246afddc5707b6c2d10745a
AdityaSid <adityasid786@gmail.com>
2022-06-10 07:33:48 +0530

    Recursive Factorial of a number

---
+++ 6.Recursive-Factorial-of-a-Number.js
@@ -0,0 +1,20 @@
+function recursiveFactorial(n) {
+    if (n < 0) {
+        return "negative number not accepted"
+    }
+
+    if (n < 2) {
+        return 1;
+    }
+
+    return n * recursiveFactorial(n - 1);
+}
+
+console.log(recursiveFactorial(0)); // 1
+console.log(recursiveFactorial(1)); // 1
+console.log(recursiveFactorial(2)); // 2
+console.log(recursiveFactorial(3)); // 6
+console.log(recursiveFactorial(4)); // 24
+console.log(recursiveFactorial(5)); // 120
+
+// Big-O = O(n)
